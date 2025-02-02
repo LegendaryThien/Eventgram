dccf78c11e027af5122acde2590a0e9769ff82f2
DerpDood21 <derek.nguyen1028@gmail.com>
2025-02-01 18:23:00 -0800

    synced lobsangs stuff

--- event-media/src/app/page.tsx
+++ event-media/src/app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-<<<<<<< HEAD
       <body className="bg-stone-900">
       <div className="m-10 bg-[#2F2F2F] text-white text-sm rounded-md p-5 border-[#3C3C3C] w-[98%] mx-auto">
                 <h1 className="text-2xl font-bold">WEBSITE NAME</h1>
@@ -32,10 +31,6 @@ export default async function Home() {
                 </div>
       </div>
       </body>
-=======
-      <main>
-      </main>
->>>>>>> e9747d7 (rsvp and comment)
     </HydrateClient>
   );
 }
